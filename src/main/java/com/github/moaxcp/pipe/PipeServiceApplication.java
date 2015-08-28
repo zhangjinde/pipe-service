5336dcf73f618ec561ed91684ff219367a946bf6
John Mercier <moaxcp@gmail.com>
2015-08-27 21:02:47 -0400

    adding code from start.spring.io

---
+++ src/main/java/com/github/moaxcp/pipe/PipeServiceApplication.java
@@ -0,0 +1,12 @@
+package com.github.moaxcp.pipe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PipeServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PipeServiceApplication.class, args);
+    }
+}
